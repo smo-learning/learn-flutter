8fb88f9901648ab6a6e9952f54066c0e11e81bde
Nils Smolinski <nils.smolinski@lyth.de>
2020-12-13 16:15:11 +0100

    Fonts and Themes

--- personal_expense_basics/lib/main.dart
+++ personal_expense_basics/lib/main.dart
@@ -13,6 +13,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        primaryTextTheme: ThemeData.light().textTheme.copyWith(
+              title: TextStyle(
+                fontFamily: 'OpenSans',
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+              button: TextStyle(color: Colors.white),
+            ),
+      ),
       home: _HomePage(),
     );
   }
--- personal_expense_basics/lib/widgets/list_expense.dart
+++ personal_expense_basics/lib/widgets/list_expense.dart
@@ -9,9 +9,18 @@ class ListExpense extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: this._expenses.map((e) => _generateCardForExpense(e)).toList(),
+    return Container(
+      height: 450,
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return _generateCardForExpense(_expenses[index]);
+        },
+        itemCount: _expenses.length,
+      ),
     );
+    // return Column(
+    //   children: this._expenses.map((e) => _generateCardForExpense(e)).toList(),
+    // );
   }
 
   Card _generateCardForExpense(Expense expense) {
@@ -22,40 +31,53 @@ class ListExpense extends StatelessWidget {
           horizontal: 10,
           vertical: 15,
         ),
-        child: Row(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.purple,
-                  width: 2,
-                ),
-              ),
-              child: Container(
-                margin: EdgeInsets.all(10),
-                child: Text(expense.amount.toString()),
+        child: ListTile(
+          leading: CircleAvatar(
+            radius: 30,
+            child: Padding(
+              padding: EdgeInsets.all(6),
+              child: FittedBox(
+                child: Text('${expense.amount}€'),
               ),
             ),
-            Spacer(),
-            Container(
-              margin: EdgeInsets.only(
-                left: 10,
-                right: 10,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                // mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  FittedBox(
-                    fit: BoxFit.fitWidth,
-                    child: Text(expense.title),
-                  ),
-                  Text(DateFormat.yMMMd().format(expense.date)),
-                ],
-              ),
-            ),
-          ],
+          ),
+          title: Text(expense.title),
+          subtitle: Text(DateFormat.yMMMd().format(expense.date)),
         ),
+        // Row(
+        //   children: [
+        //     Container(
+        //       decoration: BoxDecoration(
+        //         border: Border.all(
+        //           color: Colors.purple,
+        //           width: 2,
+        //         ),
+        //       ),
+        //       child: Container(
+        //         margin: EdgeInsets.all(10),
+        //         child: Text(expense.amount.toString()),
+        //       ),
+        //     ),
+        //     Spacer(),
+        //     Container(
+        //       margin: EdgeInsets.only(
+        //         left: 10,
+        //         right: 10,
+        //       ),
+        //       child: Column(
+        //         crossAxisAlignment: CrossAxisAlignment.end,
+        //         // mainAxisAlignment: MainAxisAlignment.end,
+        //         children: [
+        //           FittedBox(
+        //             fit: BoxFit.fitWidth,
+        //             child: Text(expense.title),
+        //           ),
+        //           Text(DateFormat.yMMMd().format(expense.date)),
+        //         ],
+        //       ),
+        //     ),
+        //   ],
+        // ),
       ),
     );
   }
