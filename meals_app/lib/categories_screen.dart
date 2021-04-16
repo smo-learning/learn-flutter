766b4a20db1710649ae50482d0dda87c9075e0ce
Nils Smolinski <nils.smolinski@lyth.de>
2021-04-16 09:10:58 +0200

    base layout

---
+++ meals_app/lib/categories_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/category_item.dart';
+
+import './dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "DeliMeal",
+        ),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map((e) => CategoryItem(e.title, e.color))
+            .toList(),
+        padding: const EdgeInsets.all(25),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
