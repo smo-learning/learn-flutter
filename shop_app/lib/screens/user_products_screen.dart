a7b7d258171db16c08f3cec7c3de41e05f993a9e
unknown <nilssmol@gmail.com>
2021-04-30 13:22:08 +0200

    Refreshindicator

--- shop_app/lib/screens/user_products_screen.dart
+++ shop_app/lib/screens/user_products_screen.dart
@@ -8,6 +8,10 @@ import 'package:shop_app/widgets/user_product_item.dart';
 class UserProductsScreen extends StatelessWidget {
   static const routeName = "/user-products";
 
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false).fetchAndSetProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
@@ -25,23 +29,26 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListView.builder(
-          itemBuilder: (_, i) {
-            final productItem = productsData.items[i];
-            return Column(
-              children: [
-                new UserProductItem(
-                  id: productItem.id,
-                  title: productItem.title,
-                  imageUrl: productItem.imageUrl,
-                ),
-                Divider()
-              ],
-            );
-          },
-          itemCount: productsData.items.length,
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListView.builder(
+            itemBuilder: (_, i) {
+              final productItem = productsData.items[i];
+              return Column(
+                children: [
+                  new UserProductItem(
+                    id: productItem.id,
+                    title: productItem.title,
+                    imageUrl: productItem.imageUrl,
+                  ),
+                  Divider()
+                ],
+              );
+            },
+            itemCount: productsData.items.length,
+          ),
         ),
       ),
     );
