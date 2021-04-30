98191fae60aa0598f4a260e95232d99b1355087e
unknown <nilssmol@gmail.com>
2021-04-30 11:33:20 +0200

    Delete Product

--- shop_app/lib/providers/products.dart
+++ shop_app/lib/providers/products.dart
@@ -83,4 +83,9 @@ class Products with ChangeNotifier {
       notifyListeners();
     }
   }
+
+  void deleteProduct(String id) {
+    _items.removeWhere((element) => element.id == id);
+    notifyListeners();
+  }
 }
